552cfa0647e43f3046cbf13837c4fd75c26a2f97
Michael Chen <tonghaoch@qq.com>
2019-10-15 15:07:31 +0800

    finish exercises 18-30

---
+++ week-01/day-02/diagonal-matrix.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// - Create (dynamically*) a two dimensional list
+//   with the following matrix**. Use a loop!
+//
+//   0 0 0 1
+//   0 0 1 0
+//   0 1 0 0
+//   1 0 0 0
+//
+// - Print this two dimensional list to the console
+//
+// * size should depend on a variable
+// ** Relax, a matrix is just like an array
+
+let size = 4;
+
+let matrix = [];
+for (let i = 0; i < size; i++) {
+  let temp = [];
+  for (let j = 0; j < size; j++) {
+    temp[j] = 0;
+  }
+  temp[size - 1 - i] = 1;
+  matrix.push(temp);
+}
+
+for (let i = 0; i < matrix.length; i++) {
+  console.log(matrix[i]);
+}
---
+++ week-01/day-02/reverse.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// - Create a variable named `aj`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Reverse the order of the elements in `aj`
+// 		- do it with the built in method
+//		- do it with creating a new temp array and a loop
+// - Print the elements of the reversed `aj`
+
+let aj = [3, 4, 5, 6, 7];
+aj.reverse();
+console.log(aj);
+
+aj = [3, 4, 5, 6, 7];
+let ja = [];
+for (let i = aj.length - 1; i >= 0; i--) {
+  ja.push(aj[i]);
+}
+console.log(ja);
