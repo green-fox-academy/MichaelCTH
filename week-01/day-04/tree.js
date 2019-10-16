fa8cf2253384b05c1bae72b159e8924de953ff64
Michael Chen <tonghaoch@qq.com>
2019-10-16 15:13:38 +0800

    add tree exercise for week1day4

---
+++ week-01/day-04/tree.js
@@ -0,0 +1,34 @@
+tree1 = {
+    'type': 'Banyan',
+    'leaf color': 'green',
+    'age': 20,
+    'sex': 'm'
+};
+
+tree2 = {
+    'type': 'Neem',
+    'leaf color': 'red',
+    'age': 140,
+    'sex': 'm'
+};
+
+tree3 = {
+    'type': 'Peepal',
+    'leaf color': 'yellow',
+    'age': 54,
+    'sex': 'f'
+};
+
+tree4 = {
+    'type': 'Aloe Vera',
+    'leaf color': 'green',
+    'age': 3,
+    'sex': 'm'
+};
+
+tree5 = {
+    'type': 'Tulsi',
+    'leaf color': 'red',
+    'age': 9,
+    'sex': 'f'
+};
