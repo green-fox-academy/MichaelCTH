467e7eff57ccdd7cbd89168e42f587503af4dcad
Michael Chen <tonghaoch@qq.com>
2019-11-04 15:17:58 +0800

    feat: complete the simple golden acorn app

---
+++ week-04/day-01/simple-golden-acorn-app/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SimpleGoldenAcornApp from './SimpleGoldenAcornApp';
+
+ReactDOM.render(<SimpleGoldenAcornApp />, document.getElementById('root'));
