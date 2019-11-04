0db8a15a9b7e5b56cd05b9efe69db110065915ab
Michael Chen <tonghaoch@qq.com>
2019-11-04 16:26:20 +0800

    feat: complete my-app and todo app

---
+++ week-04/day-01/my-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FirstComponent from './first-component';
+import SecondComponent from './second-component';
+
+//let element = <FirstComponent />;
+// let element = <SecondComponent title="My nice title" />
+let element = <SecondComponent items={[ 'a', 'b', 'c' ]} />;
+
+ReactDOM.render(element,document.getElementById('react-container'));
+
+
+//alert(element.props.children);
