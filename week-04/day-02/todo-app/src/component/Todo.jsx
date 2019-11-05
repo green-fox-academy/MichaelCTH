3faa56e64ed68edbfe0e4f2d1dca39de25ef2611
Michael Chen <tonghaoch@qq.com>
2019-11-05 23:33:29 +0800

    fix: the delete button works fine now

--- week-04/day-02/todo-app/src/component/Todo.jsx
+++ week-04/day-02/todo-app/src/component/Todo.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import ItemInput from './ItemInput';
 import ItemList from './ItemList';
 
-export default class Todo extends Component {
+class Todo extends Component {
   constructor(props) {
     super(props);
+
     this.updateUserItem = props.dataUpdate;
     this.state = props.dataRead();
-  }
 
-  getUserItem() {
-    return this.state.data.filter((i) => i.name === this.state.curUser)[0].items;
+    this.dataList = this.state.data;
+    this.user = this.dataList.filter((i) => i.name === this.state.curUser)[0];
   }
 
   itemExist(val) {
-    const items = this.getUserItem();
+    const { items } = this.user;
     let exist = false;
     items.forEach((i) => {
       if (i.name === val) {
@@ -34,36 +35,42 @@ export default class Todo extends Component {
       return;
     }
 
-    const items = this.getUserItem();
+    const { items } = this.user;
     items.push({
       name: text,
       checked: false,
     });
-    this.setState({ items });
+    // this.setState({ items });
+    this.updateUserItem(this.dataList);
     document.querySelector('input[type="text"]').value = '';
   }
 
   deleteItem(e) {
     const val = e.target.parentElement.dataset.name;
-    let items = this.getUserItem();
+    let { items } = this.user;
     items = items.filter((i) => i.name !== val);
-    this.setState({ items });
+    this.user.items = items;
+    // this.setState({ items });
+    this.updateUserItem(this.dataList);
   }
 
   checkItem(e) {
     const val = e.target.parentElement.dataset.name;
-    let items = this.getUserItem();
+    let { items } = this.user;
     items = items.map((i) => {
       if (i.name === val) {
         i.checked = !i.checked;
       }
       return i;
     });
-    this.setState({ items });
+    // this.setState({ items });
+    this.user.items = items;
+    this.updateUserItem(this.dataList);
   }
 
   render() {
-    const items = this.getUserItem();
+    const { items } = this.user;
+
     return (
       <div className="App">
         <h1 className="heading">TODOS</h1>
@@ -77,3 +84,5 @@ export default class Todo extends Component {
     );
   }
 }
+
+export default withRouter(Todo);
