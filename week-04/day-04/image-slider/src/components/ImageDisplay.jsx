0b77b017cdd6ad093160a190c6f662d1cdeda5d2
Michael Chen <tonghaoch@qq.com>
2019-11-07 22:49:40 +0800

    style: re-style the css and jsx file

--- week-04/day-04/image-slider/src/components/ImageDisplay.jsx
+++ week-04/day-04/image-slider/src/components/ImageDisplay.jsx
@@ -1,14 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 export default function ImageDisplay({ curImgIdx, images }) {
-  useEffect(() => {
-    document.querySelector('.ImgViwer').style.transition = '1s ease-in-out';
-    document.querySelector('.ImgViwer').style.opacity = 1;
-
-    return () => {
-      document.querySelector('.ImgViwer').style.opacity = 0;
-    };
-  });
   return (
     <div className="ImgViwer">
       <img src={images[curImgIdx].urls.regular} alt={images[curImgIdx].alt_description} />
--- week-04/day-04/image-slider/src/components/Search.jsx
+++ week-04/day-04/image-slider/src/components/Search.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
- Row, Col, Input, Button, Switch 
+ Row, Col, Input, Button 
 } from 'antd';
 
 export default function Search({ setSearchKey, onClick }) {
@@ -13,9 +13,9 @@ export default function Search({ setSearchKey, onClick }) {
       <Col span={6} offset={8}>
         <Input placeholder="Ex. Halloween" onChange={onInputChange} />
       </Col>
-      <Col span={1}>
+      {/* <Col span={1}>
         <Switch checkedChildren="JPG" unCheckedChildren="MP4" defaultChecked />
-      </Col>
+      </Col> */}
       <Col span={1}>
         <Button icon="search" onClick={onClick}>
           Search
