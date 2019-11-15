cd8f8e8d7ca883c8ed13b2bf02b3c754424bdcde
Michael Chen <tonghaoch@qq.com>
2019-11-15 13:09:57 +0800

    feat: add frontend app

---
+++ week-05/day-05/Frontend/app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.static('assets'));
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/doubling', (req, res) => {
+  console.log(req.query.input);
+  if (req.query.input) {
+    res.json({
+      received: req.query.input,
+      result: req.query.input * 2,
+    });
+  } else {
+    // 400
+    res.json({ error: 'Please provide an input!' });
+  }
+});
+
+app.get('/greeter', (req, res) => {
+  const { name, title } = req.query;
+  if (name && title) {
+    res.json({
+      welcome_message: 'Oh, hi there petike, my dear student!',
+    });
+  } else if (name) {
+    res.json({ error: 'Please provide a title!' });
+  } else if (title) {
+    res.json({ error: 'Please provide a name!' });
+  } else {
+    res.json({ error: 'Please provide a name and a title!' });
+  }
+});
+
+app.get('/appenda/:appendable', (req, res) => {
+  const { appendable } = req.params;
+  res.json({ appended: `${appendable}a` });
+});
+
+const factorial = (n) => ((n !== 1) ? n * factorial(n - 1) : 1);
+
+
+app.post('/dountil/:action', (req, res) => {
+  let { until } = req.body;
+  until = parseInt(until, 10);
+  const { action } = req.params;
+  if (until) {
+    if (action === 'sum') {
+      res.json({
+        result: (until + 1) * (until / 2),
+      });
+    } else if (action === 'factor') {
+      res.json({
+        result: factorial(until),
+      });
+    } else {
+      res.json({
+        error: 'Unknown action!',
+      });
+    }
+  } else {
+    res.json({
+      error: 'Please provide a number!',
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is up and running on ${PORT}`);
+});
