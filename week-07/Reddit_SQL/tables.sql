1ac2d32dce98a0a9fc6d8b526d8f1c4b0db49e05
Michael Chen <tonghaoch@qq.com>
2019-11-25 16:16:46 +0800

    fix: altered data tables to meet the needs

--- week-07/Reddit_SQL/tables.sql
+++ week-07/Reddit_SQL/tables.sql
@@ -16,14 +16,14 @@ CREATE TABLE IF NOT EXISTS Posts
   parentPost  BIGINT        UNSIGNED,
   title       VARCHAR(100)  NOT NULL,
   url         VARCHAR(2083),
-  content     VARCHAR(500),
-  createdOn   BIGINT,
+  text        VARCHAR(500),
+  timestamp   BIGINT,
   owner       BIGINT        UNSIGNED,
-  FOREIGN KEY(owner)        REFERENCES Users(id)
+  FOREIGN KEY(owner)        REFERENCES Users(id) ON DELETE CASCADE
 );
 
-ALTER TABLE Posts ADD FOREIGN KEY (masterPost) REFERENCES Posts(id);
-ALTER TABLE Posts ADD FOREIGN KEY (parentPost) REFERENCES Posts(id);
+ALTER TABLE Posts ADD FOREIGN KEY (masterPost) REFERENCES Posts(id) ON DELETE CASCADE;
+ALTER TABLE Posts ADD FOREIGN KEY (parentPost) REFERENCES Posts(id) ON DELETE CASCADE;
 
 CREATE TABLE IF NOT EXISTS Votes
 (
@@ -31,6 +31,6 @@ CREATE TABLE IF NOT EXISTS Votes
   postID      BIGINT        UNSIGNED,
   vote        TINYINT,
   PRIMARY KEY(userID,postID),
-  FOREIGN KEY(userID)       REFERENCES Users(id),
-  FOREIGN KEY(postID)       REFERENCES Posts(id)
+  FOREIGN KEY(userID)       REFERENCES Users(id) ON DELETE CASCADE,
+  FOREIGN KEY(postID)       REFERENCES Posts(id) ON DELETE CASCADE
 );
